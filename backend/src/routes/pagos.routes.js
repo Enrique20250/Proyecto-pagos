28326db5e9b12eb7967fcb75303ec1e7e3f8ca98
ZAIKOZZZ2121 <2023110521@udh.edu.pe>
2025-11-14 21:06:47 -0500

    Update pagos.routes.js

--- backend/src/routes/pagos.routes.js
+++ backend/src/routes/pagos.routes.js
@@ -1,10 +1,9 @@
-
 const express = require("express");
 const router = express.Router();
 const PagoController = require("../controllers/pagos.controller");
 
-router.get("/pagos", PagoController.listarPagos);
-router.post("/pagos", PagoController.registrarPago);
-router.get("/pagos/:id", PagoController.detallePago);
+router.get("/", PagoController.listarPagos);
+router.post("/", PagoController.registrarPago);
+router.get("/:id", PagoController.detallePago);
 
 module.exports = router;
