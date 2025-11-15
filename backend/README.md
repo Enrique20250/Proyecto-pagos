441a97155e346cbde4de05b175f792c5a2237ac6
Enrique20250 <enrique_98_78@hotmail.com>
2025-11-15 17:30:49 -0500

    Delete backend directory

--- backend/README.md
+++
@@ -1,3 +0,0 @@
-# Backend del Sistema de Pagos
-
-Este módulo contiene la lógica del servidor, las API y la integración con las pasarelas de pago (tarjeta de crédito, transferencia bancaria, etc.).
--- backend/src/routes/pagos.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const PagoController = require("../controllers/pagos.controller");
-
-router.get("/", PagoController.listarPagos);
-router.post("/", PagoController.registrarPago);
-router.get("/:id", PagoController.detallePago);
-
-module.exports = router;
