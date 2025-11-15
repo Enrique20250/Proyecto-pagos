3df710d62481c62aba8d88a9d28568a74e8d46d6
ZAIKOZZZ2121 <2023110521@udh.edu.pe>
2025-11-14 21:01:55 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PagoService = require("./servicio/PagoService");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
