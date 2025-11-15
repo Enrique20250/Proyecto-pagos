94aea23abb1431424fb22fc2379872047e994fa1
Enrique20250 <enrique_98_78@hotmail.com>
2025-11-15 17:40:50 -0500

    Delete backend directory

--- backend/controllers /pagos controller.py
+++
@@ -1,8 +0,0 @@
-def procesar_pago(datos):
-    monto = datos.get("monto")
-    metodo = datos.get("metodo")
-    return {
-        "mensaje": "Pago procesado correctamente",
-        "monto": monto,
-        "metodo": metodo
-    }
