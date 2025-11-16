0749f3ddfe6e6ee27467884cef15d42f69bba8f3
Enrique20250 <enrique_98_78@hotmail.com>
2025-11-16 11:05:55 -0500

    Update pagos controller.py

--- backend/controllers/pagos controller.py
+++ backend/controllers/pagos controller.py
@@ -1,8 +1,19 @@
-def procesar_pago(datos):
-    monto = datos.get("monto")
-    metodo = datos.get("metodo")
-    return {
-        "mensaje": "Pago procesado correctamente",
-        "monto": monto,
-        "metodo": metodo
-    }
+def procesar_tarjeta(datos_tarjeta):
+
+    numero = datos_tarjeta.get('numero')
+    fecha = datos_tarjeta.get('fecha')
+    cvv = datos_tarjeta.get('cvv')
+
+    if numero and fecha and cvv:
+        return "Pago con tarjeta procesado correctamente"
+    return "Error en datos de tarjeta"
+
+def procesar_transferencia(datos_transferencia):
+
+    banco = datos_transferencia.get('banco')
+    cuenta = datos_transferencia.get('cuenta')
+    monto = datos_transferencia.get('monto')
+
+    if banco and cuenta and monto:
+        return "Transferencia procesada correctamente"
+    return "Error en datos de transferencia"
