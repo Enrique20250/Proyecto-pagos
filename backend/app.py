3b6118fe76b9425bd273dfd610b94c82f203e6da
Enrique20250 <enrique_98_78@hotmail.com>
2025-11-16 11:07:19 -0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 from routes.pagos import pagos_bp
-from routes.tarjeta import tarjeta_bp
 from routes.transferencia import transferencia_bp
+from routes.tarjeta import tarjeta_bp
 
-app = Flask(__name__)
-app.register_blueprint(pagos_bp)
-app.register_blueprint(tarjeta_bp)
-app.register_blueprint(transferencia_bp)
+app = Flask(__name__, template_folder='templates')
+
+app.register_blueprint(pagos_bp, url_prefix="/pagos")
+app.register_blueprint(transferencia_bp, url_prefix="/transferencia")
+app.register_blueprint(tarjeta_bp, url_prefix="/tarjeta")
+
 @app.route("/")
-def inicio():
-    return "API de Pagos lista"
+def index():
+    return render_template("index.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
