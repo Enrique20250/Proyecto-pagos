8634a517ff5672337510993c2a4de3a201b11c56
Enrique20250 <enrique_98_78@hotmail.com>
2025-11-16 11:06:17 -0500

    Update pagos.py

--- backend/rooutes/pagos.py
+++ backend/rooutes/pagos.py
@@ -1,9 +1,22 @@
-from flask import Blueprint, request
-from controllers.pagos_controller import procesar_pago
+from flask import Blueprint, request, render_template, jsonify
+from controllers.pagos_controller import procesar_tarjeta, procesar_transferencia
 
-pagos_bp = Blueprint("pagos", __name__, url_prefix="/pagos")
-@pagos_bp.post("/realizar")
+pagos_bp = Blueprint('pagos', __name__, url_prefix='/pagos')
+
+@pagos_bp.route('/pagar', methods=['GET'])
+def mostrar_formulario():
+    return render_template('pagar.html')
+
+@pagos_bp.route('/realizar', methods=['POST'])
 def realizar_pago():
-    datos = request.json
-    respuesta = procesar_pago(datos)
-    return respuesta
+    datos = request.get_json()
+    metodo = datos.get('metodo')
+
+    if metodo == 'tarjeta':
+        mensaje = procesar_tarjeta(datos)
+    elif metodo == 'transferencia':
+        mensaje = procesar_transferencia(datos)
+    else:
+        mensaje = "Método de pago no soportado"
+
+    return jsonify({"mensaje": mensaje})
